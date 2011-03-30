8334d4ff8b4216476c100a2ab6acb391f4996e24
Ben Gruver <JesusFreke@JesusFreke.com>
2011-03-30 00:37:37 -0400

    Add the interface/hid/endpoint/report descriptors and associated data

--- descriptors.h
+++ descriptors.h
@@ -19,13 +19,120 @@ CONFIGURATION_DESCRIPTOR:
 /*bLength*/             .byte 0x09
 /*bDescriptorType*/     .byte DESC_CONFIGURATION
 /*wTotalLength*/        .word CONFIGURATION_END - CONFIGURATION
-/*bNumInterfaces*/      .byte 0x00
+/*bNumInterfaces*/      .byte 0x01
 /*bConfigurationValue*/ .byte 0x01
 /*iConfiguration*/      .byte 0x04
 /*bmAttributes*/        .byte 0x80
 /*bMaxPower*/           .byte 0xFA ;TODO: need to measure current draw
 CONFIGURATION_DESCRIPTOR_END:
+
+INTERFACE_DESCRIPTOR:
+/*bLength*/             .byte 0x09
+/*bDescriptorType*/     .byte DESC_INTERFACE
+/*bInterfaceNumber*/    .byte 0x00
+/*bAlternateSetting*/   .byte 0x00
+/*bNumEndpoints*/       .byte 0x01
+/*bInterfaceClass*/     .byte 0x03 ;HID class
+/*bInterfaceSubClass*/  .byte 0x00 ;no subclass (yet)
+/*bInterfaceProtocol*/  .byte 0x00 ;no protocol (yet)
+/*iInterface*/          .byte 0x05
+INTERFACE_DESCRIPTOR_END:
+
+HID_DESCRIPTOR:
+/*bLength*/             .byte 0x09
+/*bDescriptorType*/     .byte DESC_HID
+/*bcdHID*/              .word 0x0111
+/*bCountryCode*/        .byte 0x21 ;US
+/*bNumDescriptors*/     .byte 0x01
+/*bDescriptorType*/     .byte DESC_REPORT
+/*wDescriptorLength*/   .word REPORT_DESCRIPTOR_END - REPORT_DESCRIPTOR
+HID_DESCRIPTOR_END:
+
+ENDPOINT_DESCRIPTOR:
+/*bLength*/             .byte 0x07
+/*bDescriptorType*/     .byte DESC_ENDPOINT
+/*bEndpointAddress*/    .byte 0x01 | ENDPOINT_DIR_IN
+/*bmAttributes*/        .byte ENDPOINT_TYPE_INTERRUPT
+/*wMaxPacketSize*/      .word 0x20
+/*bInterval*/           .byte 1 ;1ms polling period
+ENDPOINT_DESCRIPTOR_END:
 CONFIGURATION_END:
+
+
+REPORT_DESCRIPTOR:
+;Usage page - Generic Desktop
+.byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_GLOBAL | GLOBAL_ITEM_USAGE_PAGE, \
+      HID_USAGE_PAGE_GENERIC_DESKTOP
+
+;Usage - Keyboard
+.byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_LOCAL | LOCAL_ITEM_USAGE, \
+      HID_USAGE_GENERIC_DESKTOP_KEYBOARD
+
+;Application collection item
+.byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_MAIN | MAIN_ITEM_COLLECTION, \
+    MAIN_ITEM_COLLECTION_APPLICATION
+
+    ;-----------Button Array----------
+
+    ;Report size
+    .byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_GLOBAL | GLOBAL_ITEM_REPORT_SIZE, \
+          0x08
+
+    ;Report count - support up to 20 simultaneous buttons at once
+    .byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_GLOBAL | GLOBAL_ITEM_REPORT_COUNT, \
+          0x14
+
+    ;Logical minimum
+    .byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_GLOBAL | GLOBAL_ITEM_LOGICAL_MINIMUM, \
+          0x00
+    ;Logical maximum
+    .byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_GLOBAL | GLOBAL_ITEM_LOGICAL_MAXIMUM, \
+          0xDD
+
+    ;Usage page - Keyboard
+    .byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_GLOBAL | GLOBAL_ITEM_USAGE_PAGE, \
+          HID_USAGE_PAGE_KEYBOARD
+
+    ;Usage minimum
+    .byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_LOCAL | LOCAL_ITEM_USAGE_MINIMUM, \
+          0x00
+    ;Usage maximum
+    .byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_LOCAL | LOCAL_ITEM_USAGE_MAXIMUM, \
+          0xDD
+
+    ;Input item (array)
+    .byte HID_ITEM_DATA_SIZE_0 | HID_ITEM_TYPE_MAIN | MAIN_ITEM_INPUT
+
+    ;------------Modifier List---------
+
+    ;Report size
+    .byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_GLOBAL | GLOBAL_ITEM_REPORT_SIZE, \
+          0x01
+
+    ;Report count - support up to 20 simultaneous buttons at once
+    .byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_GLOBAL | GLOBAL_ITEM_REPORT_COUNT, \
+          0x08
+
+    ;Logical maximum
+    .byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_GLOBAL | GLOBAL_ITEM_LOGICAL_MAXIMUM, \
+          0x01
+
+    ;Usage minimum
+    .byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_LOCAL | LOCAL_ITEM_USAGE_MINIMUM, \
+          0xE0
+    ;Usage maximum
+    .byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_LOCAL | LOCAL_ITEM_USAGE_MAXIMUM, \
+          0xE7
+
+    ;Input item (variable)
+    .byte HID_ITEM_DATA_SIZE_1 | HID_ITEM_TYPE_MAIN | MAIN_ITEM_INPUT, \
+          MAIN_ITEM_VARIABLE
+
+;End application collection
+.byte HID_ITEM_DATA_SIZE_0 | HID_ITEM_TYPE_MAIN | MAIN_ITEM_END_COLLECTION
+
+
+REPORT_DESCRIPTOR_END:
 
 .macro string_descriptor index, string
     ;calculate the length of the string
@@ -67,6 +174,8 @@ string_descriptor 2, "DataHand"
 string_descriptor 3, "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938"
 ;Configuration Name
 string_descriptor 4, "The Configuration of DOOOOOOOOM"
+;Interface Name
+string_descriptor 5, "Keyboard Interface"
 
 ;we have to be aligned, for any code that follows
 .align 2
--- usb.h
+++ usb.h
@@ -20,6 +20,9 @@
 #define DESC_DEVICE_QUALIFIER           6
 #define DESC_OTHER_SPEED_CONFIGURATION  7
 #define DESC_INTERFACE_POWER            8
+#define DESC_HID                        0x21
+#define DESC_REPORT                     0x22
+#define DESC_PHYSICAL                   0x23
 
 /*Feature values for CLEAR_FEATURE and SET_FEATURE requests*/
 #define FEATURE_ENDPOINT_HALT           0
@@ -44,6 +47,119 @@
 #define STATE_DEFAULT
 #define STATE_ADDRESS
 #define STATE_CONFIGURED
+
+/*Endpoint direction for endpoint descriptor field bEndpointAddress*/
+#define ENDPOINT_DIR_OUT 0x00
+#define ENDPOINT_DIR_IN  0x80
+
+/*Endpoint transfer type for endpoint descriptor field bmAttributes*/
+#define ENDPOINT_TYPE_CONTROL       0x00
+#define ENDPOINT_TYPE_ISOCHRONOUS   0x01
+#define ENDPOINT_TYPE_BULK          0x02
+#define ENDPOINT_TYPE_INTERRUPT     0x03
+
+/*Endpoint synchronization type for endpoint descriptor field bmAttributes
+Only for use with isochronous endpoints*/
+#define ENDPOINT_SYNCH_NONE         0x00 << 2
+#define ENDPOINT_SYNCH_ASYNC        0x01 << 2
+#define ENDPOINT_SYNCH_ADAPTIVE     0x02 << 2
+#define ENDPOINT_SYNCH_SYNCHRONOUS  0x03 << 2
+
+/*Endpoint usage type for endpoint descriptor field bmAttributes
+Only for use with isochronous endpoints*/
+#define ENDPOINT_USAGE_DATA                 0x00 << 4
+#define ENDPOINT_USAGE_FEEDBACK             0x01 << 4
+#define ENDPOINT_USAGE_IMPLICIT_FEEDBACK    0x02 << 4
+#define ENDPOINT_USAGE_RESERVED             0x03 << 4
+
+/*Usage pages*/
+#define HID_USAGE_PAGE_GENERIC_DESKTOP      0x01
+#define HID_USAGE_PAGE_KEYBOARD             0x07
+
+/*Generic desktop usages*/
+#define HID_USAGE_GENERIC_DESKTOP_KEYBOARD  0x06
+
+/*Data size definitions for HID items*/
+#define HID_ITEM_DATA_SIZE_0       0x00
+#define HID_ITEM_DATA_SIZE_1       0x01
+#define HID_ITEM_DATA_SIZE_2       0x02
+#define HID_ITEM_DATA_SIZE_4       0x04
+
+/*HID item types*/
+#define HID_ITEM_TYPE_MAIN          0x00 << 2
+#define HID_ITEM_TYPE_GLOBAL        0x01 << 2
+#define HID_ITEM_TYPE_LOCAL         0x02 << 2
+
+/*HID main item tags*/
+#define MAIN_ITEM_INPUT             0x08 << 4
+#define MAIN_ITEM_OUTPUT            0x09 << 4
+#define MAIN_ITEM_COLLECTION        0x0A << 4
+#define MAIN_ITEM_FEATURE           0x0B << 4
+#define MAIN_ITEM_END_COLLECTION    0x0C << 4
+
+/*First byte of main item data*/
+#define MAIN_ITEM_DATA              0
+#define MAIN_ITEM_CONSTANT          0b00000001
+
+#define MAIN_ITEM_ARRAY             0
+#define MAIN_ITEM_VARIABLE          0b00000010
+
+#define MAIN_ITEM_ABSOLUTE          0
+#define MAIN_ITEM_RELATIVE          0b00000100
+
+#define MAIN_ITEM_NOWRAP            0
+#define MAIN_ITEM_WRAP              0b00001000
+
+#define MAIN_ITEM_LINEAR            0
+#define MAIN_ITEM_NONLINEAR         0b00010000
+
+#define MAIN_ITEM_PREFERRED         0
+#define MAIN_ITEM_NONPREFERRED      0b00100000
+
+#define MAIN_ITEM_NONULL            0
+#define MAIN_ITEM_NULL              0b01000000
+
+#define MAIN_ITEM_NONVOLATILE       0
+#define MAIN_ITEM_VOLATILE          0b10000000
+
+/*Second byte of main item data*/
+#define MAIN_ITEM_BITFIELD          0
+#define MAIN_ITEM_BUFFEREDBYTES     0b00000001
+
+/*Data values for collection main item*/
+#define MAIN_ITEM_COLLECTION_PHYSICAL       0x00
+#define MAIN_ITEM_COLLECTION_APPLICATION    0x01
+#define MAIN_ITEM_COLLECTION_LOGICAL        0x02
+#define MAIN_ITEM_COLLECTION_REPORT         0x03
+#define MAIN_ITEM_COLLECTION_NAMEDARRAY     0x04
+#define MAIN_ITEM_COLLECTION_USAGESWITCH    0x05
+#define MAIN_ITEM_COLLECTION_USAGEMODIFIER  0x06
+
+/*Global item tags*/
+#define GLOBAL_ITEM_USAGE_PAGE          0x00 << 4
+#define GLOBAL_ITEM_LOGICAL_MINIMUM     0x01 << 4
+#define GLOBAL_ITEM_LOGICAL_MAXIMUM     0x02 << 4
+#define GLOBAL_ITEM_PHYSICAL_MINIMUM    0x03 << 4
+#define GLOBAL_ITEM_PHYSICAL_MAXIMUM    0x04 << 4
+#define GLOBAL_ITEM_UNIT_EXPONENT       0x05 << 4
+#define GLOBAL_ITEM_UNIT                0x06 << 4
+#define GLOBAL_ITEM_REPORT_SIZE         0x07 << 4
+#define GLOBAL_ITEM_REPORT_ID           0x08 << 4
+#define GLOBAL_ITEM_REPORT_COUNT        0x09 << 4
+#define GLOBAL_ITEM_PUSH                0x0A << 4
+#define GLOBAL_ITEM_POP                 0x0B << 4
+
+/*Local item tags*/
+#define LOCAL_ITEM_USAGE                0x00 << 4
+#define LOCAL_ITEM_USAGE_MINIMUM        0x01 << 4
+#define LOCAL_ITEM_USAGE_MAXIMUM        0x02 << 4
+#define LOCAL_ITEM_DESIGNATOR_INDEX     0x03 << 4
+#define LOCAL_ITEM_DESIGNATOR_MINIMUM   0x04 << 4
+#define LOCAL_ITEM_DESIGNATOR_MAXIMUM   0x05 << 4
+#define LOCAL_ITEM_STRING_INDEX         0x07 << 4
+#define LOCAL_ITEM_STRING_MINIMUM       0x08 << 4
+#define LOCAL_ITEM_STRING_MAXIMUM       0x09 << 4
+#define LOCAL_ITEM_DELIMITER            0x0A << 4
 
 
 .macro usb_wait_for_txini tempreg
