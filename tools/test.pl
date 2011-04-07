dab40738785a1add1f0cfd8eb587aa642123caf2
Ben Gruver <jesusfreke@jesusfreke.com>
2011-04-06 23:15:19 -0400

    Add tools dir, with test script and udev rule

---
+++ tools/test.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+use strict;
+
+use Device::USB;
+
+my $usb = Device::USB->new();
+my $dev = $usb->find_device( 0xFEED, 0xFACE );
+
+$dev->open();
+$dev->detach_kernel_driver_np(0);
+
+
+#hid set idle
+#$dev->control_msg( 0b00100001, 0x0a, 0x1300, 0, undef, 0, 1000);
+
+
+my($buffer) = "";
+
+#hid get report
+my($ret) = $dev->control_msg( 0b10100001, 0x01, 0, 0, $buffer, 34, 1000);
+
+#hid get idle
+#my($ret) = $dev->control_msg( 0b10100001, 0x02, 0, 0, $buffer, 1, 1000);
+
+print "ret: $ret\n";
+print "data length: " . length($buffer) . "\n";
+print "data: " . join(", ", (unpack("C*", $buffer))) . "\n";
