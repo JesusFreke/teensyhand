ecc9556cf520c7bb412683229cb09f64fbaffae8
Ben Gruver <jesusfreke@jesusfreke.com>
2011-04-12 02:22:50 -0400

    Add implementation for normal mode key

--- main.S.pl
+++ main.S.pl
@@ -489,7 +489,7 @@ $action_map{"rgui"} = modifier_keycode(0xe7);
 $action_map{"nas"} = nas_action();
 $action_map{"naslock"} = naslock_action();
 $action_map{"func"} = undefined_action();
-$action_map{"norm"} = undefined_action();
+$action_map{"norm"} = norm_action();
 
 for (my($key_map_index)=0; $key_map_index<scalar(@key_maps); $key_map_index++) {
     my($key_map) = $key_maps[$key_map_index];
@@ -957,6 +957,40 @@ sub naslock_action {
             _sts current_press_table, r16;
             _sts persistent_mode_press_table, r16;
             _ldi r16, hi8("press_table_1");
+            _sts "current_press_table + 1", r16;
+            _sts "persistent_mode_press_table + 1", r16;
+
+            _ret;
+        };
+
+        emit_sub $release_label, sub {
+            _ret;
+        };
+
+        return [$release_label, $press_label];
+    }
+}
+
+sub norm_action {
+    return sub {
+        my($button_index) = shift;
+
+        my($press_label) = "norm_press_action_$action_count";
+        my($release_label) = "norm_release_action_$action_count";
+        $action_count++;
+
+        emit_sub $press_label, sub {
+            #store the address for the release routine
+            _ldi r16, lo8(pm($release_label));
+            _sts "release_table + " . ($button_index * 2), r16;
+            _ldi r16, hi8(pm($release_label));
+            _sts "release_table + " . (($button_index * 2) + 1), r16;
+
+            #update the press table pointers to point to the nas press table
+            _ldi r16, lo8("press_table_0");
+            _sts current_press_table, r16;
+            _sts persistent_mode_press_table, r16;
+            _ldi r16, hi8("press_table_0");
             _sts "current_press_table + 1", r16;
             _sts "persistent_mode_press_table + 1", r16;
 
